a9a83942f3c77ef623d498c83913a676beef326e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-11-27 15:19:20 +0000

    deploy

--- vue-remark/src/pages/Index.vue
+++ vue-remark/src/pages/Index.vue
@@ -1,5 +1,13 @@
 <template>
   <Layout>
+
+<h2>Clases</h2>
+
+<h2>tareas</h2>
+
+
+<h2>Publicaciones</h2>
+
 <div v-for="edge in $page.posts.edges" :key="edge.node.id">
   <li>
     <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
