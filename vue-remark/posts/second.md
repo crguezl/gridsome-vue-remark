40987c199b3a69d7778768894f28c46b4ccaa409
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-11-27 14:55:54 +0000

    deploy

---
+++ vue-remark/posts/second.md
@@ -0,0 +1,20 @@
+---
+title: "Second post!"
+excerpt: "Our second post with gridsome"
+age: 67
+---
+
+I am already in **Gridsome**!
+
+The title is:
+
+#### {{ $frontmatter.title }}
+
+The excerpt is:
+
+> {{ $frontmatter.excerpt }}
+
+
+The age is:
+
+> {{ $frontmatter.age }}
--- vue-remark/src/pages/Index.vue
+++ vue-remark/src/pages/Index.vue
@@ -1,9 +1,29 @@
 <template>
   <Layout>
-
+<div v-for="edge in $page.posts.edges" :key="edge.node.id">
+  <li>
+    <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
+  </li>
+</div>
 
   </Layout>
 </template>
+
+<page-query>
+
+query posts {
+  posts: allPost {
+    edges {
+      node {
+        title
+        excerpt
+        path
+        id
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
