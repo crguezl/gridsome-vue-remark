4abbff94b39d032cd50d5e1b4c63fb05ab23339f
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-11-27 15:22:00 +0000

    deploy

---
+++ vue-remark/src/pages/Blog.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout>
+<div v-for="edge in $page.posts.edges" :key="edge.node.id">
+  <li>
+    <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
+  </li>
+</div>
+
+  </Layout>
+</template>
+
+<page-query>
+
+query posts {
+  posts: allPost {
+    edges {
+      node {
+        title
+        excerpt
+        path
+        id
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Published Articles'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
